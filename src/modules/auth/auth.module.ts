f4cccdb01618d12557670cdae9da17f007f47f99
vasili <frugas@list.ru>
2024-05-20 13:05:44 +0300

    add user.service

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtAuthGuard } from './jwt-auth.guard';
 
 @Module({
   imports: [
@@ -16,7 +17,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
       }),
     }),
   ],
-  providers: [AuthService],
-  exports: [AuthService],
+  providers: [AuthService, JwtAuthGuard],
+  exports: [AuthService, JwtModule],
 })
 export class AuthModule {}
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,8 +1,56 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/createUser.dto';
 import { AuthService } from '../auth/auth.service';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserEntity } from './entities/User.entity';
+import { Repository } from 'typeorm';
+import { AuthUserDto } from '@modules/auth/dto/authUser.dto';
 
 @Injectable()
 export class UserService {
-  constructor(private authService: AuthService) {}
+  constructor(
+    @InjectRepository(UserEntity)
+    private userRepository: Repository<UserEntity>,
+    private authService: AuthService,
+  ) {}
+
+  private async validateUser(email: string) {
+    const candidate = await this.getUserByEmail(email);
+
+    if (candidate) {
+      return new HttpException(
+        'Пользователь с таким email существует',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+  }
+
+  async authUser(authUserDto: AuthUserDto) {
+    const candidate = await this.getUserByEmail(authUserDto.email);
+
+    if (!candidate) {
+      return new HttpException(
+        'Пользователь с таким email не существует',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    const token = await this.authService.generateToken(candidate);
+    return { ...candidate, ...token };
+  }
+
+  async registrationUser(createUserDto: CreateUserDto) {
+    await this.validateUser(createUserDto.email);
+
+    const user = await this.userRepository.save(createUserDto);
+    const token = await this.authService.generateToken(user);
+    return { ...user, ...token };
+  }
+
+  async getUserByEmail(email: string) {
+    const user = await this.userRepository.findOne({
+      where: { email },
+    });
+    return user;
+  }
 }
