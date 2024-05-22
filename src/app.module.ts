43d01a1967eceb72f6f5103c5722df9799e2383d
vasili <frugas@list.ru>
2024-05-22 21:33:09 +0300

    nest g res task

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './modules/user/user.module';
+import { TaskModule } from '@modules/task/task.module';
 
 @Module({
   imports: [
@@ -26,6 +27,7 @@ import { UserModule } from './modules/user/user.module';
       }),
     }),
     UserModule,
+    TaskModule,
   ],
   controllers: [],
   providers: [],
